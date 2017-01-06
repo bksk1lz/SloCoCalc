20ea2814fed23303c0029330ee2a9dd3c3ae1283
bksk1lz <bcking21@gmail.com>
2017-01-06 18:22:57 -0500

    upgrade to python3, add command line prompts

--- ScoreCalculator.py
+++ ScoreCalculator.py
@@ -4,16 +4,24 @@ Created on Mon Jan 25 17:01:04 2016
 
 @author: bckin_000
 """
-from points import *
-from slocos import *
+import os
+import csv
+import points
+import slocos
 
-[namelistm, namelistw, racenumlist] = get_points()
+userpath = input('Enter path to results files: ')
+filelist = [userpath + file for file in os.listdir(userpath)
+            if file.endswith('.xml')]
+
+SeasonSize = int(input('Enter the number of races to count: '))
+
+[namelistm, namelistw, racenumlist] = points.get_points(userpath, filelist)
 
 namelistm.pop(0)
-namelistw.pop(0)   
+namelistw.pop(0)
 
-#Meetdirector Bonuses!
-mds = open('MeetDirectors.txt', 'r')
+# Meetdirector Bonuses!
+mds = open(userpath + '/MeetDirectors.txt', 'r')
 mdsList = mds.read().split('\n')
 mds.close()
 
@@ -21,51 +29,54 @@ mds.close()
 for i, md in enumerate(mdsList):
     for dct in namelistm:
         if md in dct['name']:
-            dct[racenumlist[i+1]] = sorted(dct.itervalues(), reverse = True)[1]
-            
+            dct[racenumlist[i + 1]] = sorted([v for v in dct.values()
+                                              if isinstance(v, int)],
+                                             reverse=True)[0]
+
 for i, md in enumerate(mdsList):
     for dct in namelistw:
         if md in dct['name']:
-            dct[racenumlist[i+1]] = sorted(dct.itervalues(), reverse = True)[1]
+            dct[racenumlist[i + 1]] = sorted([v for v in dct.values()
+                                              if isinstance(v, int)],
+                                             reverse=True)[0]
 
-racenumlist.append('best') 
-racenumlist.append('SloCo') 
-    
-[allnames, allslocos] = get_slocos()
+racenumlist.append('best')
+racenumlist.append('SloCo')
 
-#number of races to count plus 1
-SeasonSize = 9
+[allnames, allslocos] = slocos.get_slocos(filelist)
 
-#Men's and women's dictionaries now full of everybody's names and points for each race
-#we need to get their best *SeasonSize* scores
+# Men's and women's dictionaries now full of everybody's names and points for each race
+# we need to get their best *SeasonSize* scores
 from operator import itemgetter
-             
+
 for entry in namelistm:
-    bestm = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:SeasonSize])
+    bestm = sorted([v for v in entry.values() if isinstance(v, int)],
+                   reverse=True)[0:SeasonSize]
     # sorts all dictionary values into a list. The text entry (person's name) is first
-    # the best *SeasonSize* points values are 1 - 12 in the list. they are stored in the dictionary
-    # bestm
-    entry['best'] = sum(bestm.itervalues())
-    #sum of the 11 best entries is added to each runners dictionary under the entry "sumbest11"
+    # the best *SeasonSize* points values are 1 - 12 in the list
+    entry['best'] = sum(bestm)
+    # sum of the 11 best entries is added to each runners dictionary under the
+    # entry "sumbest11"
     try:
-        entry['SloCo'] = allslocos[allnames.index(entry['name'])]    
+        entry['SloCo'] = allslocos[allnames.index(entry['name'])]
     except ValueError:
         entry['SloCo'] = 0
-#Now do the same thing for women's
+# Now do the same thing for women's
 for entry in namelistw:
-    bestw = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:SeasonSize])
-    entry['best'] = sum(bestw.itervalues())
+    bestw = sorted([v for v in entry.values() if isinstance(v, int)],
+                   reverse=True)[0:SeasonSize]
+    entry['best'] = sum(bestw)
     entry['SloCo'] = allslocos[allnames.index(entry['name'])]
-        
 
-import csv
-#write output .csv file.
-#men's and women's dicts are just written consecutively for now
-#would like to write this direct to google drive someday!
-outfile = open("output.csv", "w")
-w = csv.DictWriter(outfile, fieldnames = racenumlist, 
+
+# write output .csv file.
+# men's and women's dicts are just written consecutively for now
+# would like to write this direct to google drive someday!
+outfile = open(userpath + "output.csv", "w")
+w = csv.DictWriter(outfile, fieldnames=racenumlist,
                    restval=0, extrasaction='ignore', dialect='excel')
 
+w.writeheader()
 for entry in namelistm:
     w.writerow(entry)
 for entry in namelistw:
--- slocos.py
+++ slocos.py
@@ -1,5 +1,6 @@
 
 def get_sec(s):
+    '''Returns time in seconds for input, s, of form HH:MM:SS '''
     l = s.split(':')
     if len(l) == 1:
         return int(l[0])
@@ -7,130 +8,97 @@ def get_sec(s):
         return int(int(l[0]) * 60 + int(l[1]))
     else:
         return int(int(l[0]) * 3600 + int(l[1]) * 60 + int(l[2]))
-def get_slocos():
-    import os
-    filelist = [] #Initialize list of files in directory
-    
-    #Get a list of all .xml files in directory
-    for file in os.listdir(os.getcwd()):
-        if file.endswith(".xml"):
-            filelist.append(file)
-    
-    
-    
-    #From here on we are doing mens and womens results in parallel
-    namelist = [{}] #Initialize dictionary list of names and race times
-    
-    
-    #Now we are ready to go through the results files and write everyone's point values
-    #to their entry in namelist
+
+
+def get_slocos(filelist):
+
+    # Men and women all go in one list since were handicapping absolute times
+    namelist = [{}]  # Initialize dictionary list of names and race times
+
+    # Now we are ready to go through the results files and write everyone's point values
+    # to their entry in namelist
     import xml.etree.ElementTree as ET
     racenumlist = ["name"]
-    
+
     # Load each file and make string for the race number
     for i, file in enumerate(filelist):
         tree = ET.parse(file)
         root = tree.getroot()
-        racenumstr = "race" + str(i+1)
-        racenumlist.append(racenumstr)#builds a list of strings corresponding to the sequence of races
-        
-        #take only the first class result in file. (sometimes there's a reverse course or something at the end)
+        racenumstr = "race" + str(i + 1)
+        # builds a list of strings corresponding to the sequence of races
+        racenumlist.append(racenumstr)
+
+        # take only the first class result in file. (sometimes there's a
+        # reverse course or something at the end)
         for ClassResult in root.find("ClassResult"):
-            #Now we're in a loop iterating through the person results
+            # Now we're in a loop iterating through the person results
             for PersonResult in ClassResult.iter("PersonResult"):
-                
+
                 for ResultPosition in PersonResult.iter('ResultPosition'):
                     rpos = ResultPosition.text
-                    
+
                 if rpos is None:
                     break
-                
+
                 namestr = ""
-                x = 0 # A variable to keep track of whether the runner's name already exists in namelist
-    
-                #Write the runners name to namestr
+                x = 0  # A variable to keep track of whether the runner's name already exists in namelist
+
+                # Write the runners name to namestr
                 for Given in PersonResult.iter("Given"):
                     namestr = namestr + Given.text + " "
-                                
+
                 for Family in PersonResult.iter("Family"):
-                    namestr = namestr + Family.text 
-    
+                    namestr = namestr + Family.text
+
                 for Time in PersonResult.findall("./Result/Time"):
                     timestr = Time.text
                     timeval = get_sec(timestr)
                     break
-                         
+
                 for entry in namelist:
-                    
-                    if namestr in entry.values(): 
+
+                    if namestr in entry.values():
                         entry[racenumstr] = timeval
                         x = 1
-                        
+
                 if x == 0:
-                    namelist.append({"name":namestr, 
-                                    racenumstr:timeval})
-    
+                    namelist.append({"name": namestr,
+                                     racenumstr: timeval})
+
                 Time.clear()
                 timeval = None
                 ResultPosition.clear()
-                rpos = None            
-    
+                rpos = None
+
     racenumlist.pop(0)
     namelist.pop(0)
-    
-    
-    for i,key in enumerate(racenumlist):
+
+    for i, key in enumerate(racenumlist):
         racetimeslist = []
         for entry in namelist:
             if key in entry:
                 racetimeslist.append(entry[key])
-        
+
         wintime = float(min(racetimeslist))
-        
+
         for entry in namelist:
             if key in entry:
                 newkey = 'SloCo{0}'.format(i)
                 entry[key] = float(entry[key]) / wintime
-        
-    
-    
+
     allnames = []
     allslocos = []
-                
+
     for entry in namelist:
-        best11 = sorted(entry.values(), reverse = True)[1:18]
-    
-        midpoint = int(len(best11)/2)
-        
-        if not len(best11) % 2:
-            entry['SloCo'] = sum(best11[(midpoint - 1):(midpoint + 1)]) / 2
+        orderedslocos = sorted(
+            [v for v in entry.values() if isinstance(v, float)])
+        midpoint = int(len(orderedslocos) / 2)
+
+        if not len(orderedslocos) % 2:
+            entry['SloCo'] = sum(
+                orderedslocos[(midpoint - 1):(midpoint + 1)]) / 2
         else:
-            entry['SloCo'] = best11[midpoint]
+            entry['SloCo'] = orderedslocos[midpoint]
         allnames.append(entry['name'])
         allslocos.append(entry['SloCo'])
     return [allnames, allslocos]
-'''
-#Now do the same thing for women's
-for entry in namelistw:
-    best11w = dict(sorted(entry.iteritems(), key = itemgetter(1), reverse = True)[1:12])
-    entry['sumbest11'] = sum(best11w.itervalues())
-    
-
-racenumlist.append('sumbest11') 
-#adds the sumbest11 entry title to racenumlist for inclusion in csv output
-#csv header is not working right now for some reason
-
-import csv
-#write output .csv file.
-#men's and women's dicts are just written consecutively for now
-#would like to write this direct to google drive someday!
-outfile = open("output.csv", "w")
-w = csv.DictWriter(outfile, fieldnames = racenumlist, 
-                   restval=0, extrasaction='ignore', dialect='excel')
-
-for entry in namelistm:
-    w.writerow(entry)
-for entry in namelistw:
-    w.writerow(entry)
-outfile.close()
-'''
